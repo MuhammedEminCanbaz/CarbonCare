175118060c723403bf9ccfcbb327f70292601e05
Nergiz Alıci <159393297+nergizal@users.noreply.github.com>
2025-05-06 18:43:12 +0300

    Update main.py
    some changes done

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import models
 from database import engine
 from routers import auth, footprint, ai
 
-# Veritabanı tablolarını oluştur
+
 models.Base.metadata.create_all(bind=engine)
 
-# FastAPI uygulaması
+
 app = FastAPI()
 
-# Router'ları bağla
+
 app.include_router(auth.router)
 app.include_router(footprint.router)
 app.include_router(ai.router)
 
-# Basit kök endpoint
+
 @app.get("/")
 def root():
     return {"message": "Welcome to CarbonCare API 🌱"}
